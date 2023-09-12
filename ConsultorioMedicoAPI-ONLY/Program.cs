b23b137068a9dbfbafb939c1d671be8143d8d7cd
LopStevn <stevenlpz03@gmail.com>
2023-09-12 10:01:46 -0600

    RolController - Completado con todos los endpoints de crud

--- ConsultorioMedicoAPI-ONLY/Program.cs
+++ ConsultorioMedicoAPI-ONLY/Program.cs
@@ -1,3 +1,5 @@
+using Consultorio_Medico.loC;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddDependecies(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -19,6 +21,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+
 
 app.MapControllers();
 
