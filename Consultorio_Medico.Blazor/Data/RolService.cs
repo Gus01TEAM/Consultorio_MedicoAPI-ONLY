2075600633361bf6deaacb9db8cc3dc5e1b069b1
LopStevn <stevenlpz03@gmail.com>
2023-10-16 14:36:05 -0600

    Steven Lopez: Agregando Proyecto Web - RolService e Blazor

---
+++ Consultorio_Medico.Blazor/Data/RolService.cs
@@ -0,0 +1,89 @@
+﻿using Consultorio_Medico.BL.DTOs.RolDTO;
+using ConsultorioMedicoAPI_ONLY.DTOGenericResponse;
+using System.Text.Json;
+
+namespace Consultorio_Medico.Blazor.Data
+{
+    public class RolService
+    {
+        readonly HttpClient _httpClientAPI;
+
+        public RolService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientAPI = httpClientFactory.CreateClient("MEDICOAPI");
+        }
+
+        public async Task<DTOGenericResponse<List<RolSearchingOutputDTO>>> GetAll()
+        {
+            var response = await _httpClientAPI.GetFromJsonAsync<DTOGenericResponse<List<RolSearchingOutputDTO>>>("/api/Rol");
+
+            if (response != null)
+            {
+                return response;
+            }
+            else
+            {
+                throw new Exception("La solicitud GET no fue exitosa.");
+            }
+        }
+
+        public async Task<DTOGenericResponse<RolSearchingOutputDTO>> GetById(int id)
+        {
+            var response = await _httpClientAPI.GetFromJsonAsync<DTOGenericResponse<RolSearchingOutputDTO>>($"/api/Rol/{id}");
+
+            if (response != null)
+            {
+                return response;
+            }
+            else
+            {
+                throw new Exception("La solicitud GET no fue exitosa.");
+            }
+        }
+
+        public async Task<DTOGenericResponse<RolSearchingOutputDTO>> Create(RolInputDTO createRolDTO)
+        {
+            var response = await _httpClientAPI.PostAsJsonAsync("/api/Rol", createRolDTO);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var responseBody = await response.Content.ReadAsStringAsync();
+                return JsonSerializer.Deserialize<DTOGenericResponse<RolSearchingOutputDTO>>(responseBody);
+            }
+            else
+            {
+                throw new Exception("La solicitud POST no fue exitosa.");
+            }
+        }
+
+        public async Task<DTOGenericResponse<RolSearchingOutputDTO>> Edit(int id, RolInputDTO editRolDTO)
+        {
+            var response = await _httpClientAPI.PutAsJsonAsync($"/api/Rol/{id}", editRolDTO);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var responseBody = await response.Content.ReadAsStringAsync();
+                return JsonSerializer.Deserialize<DTOGenericResponse<RolSearchingOutputDTO>>(responseBody);
+            }
+            else
+            {
+                throw new Exception("La solicitud PUT no fue exitosa.");
+            }
+        }
+
+        public async Task<DTOGenericResponse<RolSearchingOutputDTO>> Delete(int id)
+        {
+            var response = await _httpClientAPI.DeleteAsync($"/api/Rol/{id}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var responseBody = await response.Content.ReadAsStringAsync();
+                return JsonSerializer.Deserialize<DTOGenericResponse<RolSearchingOutputDTO>>(responseBody);
+            }
+            else
+            {
+                throw new Exception("La solicitud DELETE no fue exitosa.");
+            }
+        }
+    }
+}
---
+++ Consultorio_Medico.Blazor/Program.cs
@@ -0,0 +1,37 @@
+using Consultorio_Medico.Blazor.Data;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+
+builder.Services.AddSingleton<RolService>();
+
+builder.Services.AddHttpClient("MEDICOAPI", c =>
+{
+    c.BaseAddress = new Uri(builder.Configuration["UrlsAPI:API"]);
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
