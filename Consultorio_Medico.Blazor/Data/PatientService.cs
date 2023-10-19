c63dfc693ff301137e4383a93c88f480f4acfbfa
LopStevn <stevenlpz03@gmail.com>
2023-10-18 18:12:35 -0600

    Steven Lopez - Vistas de Rol y comenzando PatientService

---
+++ Consultorio_Medico.Blazor/Data/PatientService.cs
@@ -0,0 +1,29 @@
+﻿using Consultorio_Medico.BL.DTOs.PatientDTO;
+using ConsultorioMedicoAPI_ONLY.DTOGenericResponse;
+
+namespace Consultorio_Medico.Blazor.Data
+{
+    public class PatientService
+    {
+        readonly HttpClient _httpClientAPI;
+
+        public PatientService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientAPI = httpClientFactory.CreateClient("MEDICOAPI");
+        }
+
+        public async Task<DTOGenericResponse<List<patientSearchOutputDTO>>> Search()
+        {
+            var response = await _httpClientAPI.GetFromJsonAsync<DTOGenericResponse<List<patientSearchOutputDTO>>>("/api/Rol");
+
+            if (response != null)
+            {
+                return response;
+            }
+            else
+            {
+                throw new Exception("La solicitud GET no fue exitosa.");
+            }
+        }
+    }
+}
