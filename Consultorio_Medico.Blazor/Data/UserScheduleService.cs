ff958765d0f8b1ac09db56cba0df114bc62e6f80
Gustavo Munoz <softwareadolfo@gmail.com>
2023-11-05 17:14:18 -0600

    Gustavo Muñoz: UserScheduleService Done

---
+++ Consultorio_Medico.Blazor/Data/UserScheduleService.cs
@@ -0,0 +1,58 @@
+﻿using Consultorio_Medico.BL.DTOs.RolDTO;
+using Consultorio_Medico.BL.DTOs.UserSchedule;
+using ConsultorioMedicoAPI_ONLY.DTOGenericResponse;
+using System.Text.Json;
+
+namespace Consultorio_Medico.Blazor.Data
+{
+    public class UserScheduleService
+    {
+        readonly HttpClient _httpClientAPI;
+
+        public UserScheduleService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientAPI = httpClientFactory.CreateClient("MEDICOAPI");
+        }
+        public async Task<DTOGenericResponse<List<UserScheduleSearchOutputDTO>>> Search()
+        {
+            var response = await _httpClientAPI.GetFromJsonAsync<DTOGenericResponse<List<UserScheduleSearchOutputDTO>>>("/api/UserSchedule");
+
+            if (response != null)
+            {
+                return response;
+            }
+            else
+            {
+                throw new Exception("La solicitud GET no fue exitosa.");
+            }
+        }
+
+        public async Task<DTOGenericResponse<UserScheduleSearchOutputDTO>> GetById(int id)
+        {
+            var response = await _httpClientAPI.GetFromJsonAsync<DTOGenericResponse<UserScheduleSearchOutputDTO>>($"/api/UserSchedule/{id}");
+
+            if (response != null)
+            {
+                return response;
+            }
+            else
+            {
+                throw new Exception("La solicitud GET no fue exitosa.");
+            }
+        }
+        public async Task<DTOGenericResponse<UserScheduleSearchOutputDTO>> Create(UserScheduleSearchInpuntDTO createUserSchedule)
+        {
+            var response = await _httpClientAPI.PostAsJsonAsync("/api/UserSchedule", createUserSchedule);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var responseBody = await response.Content.ReadAsStringAsync();
+                return JsonSerializer.Deserialize<DTOGenericResponse<UserScheduleSearchOutputDTO>>(responseBody);
+            }
+            else
+            {
+                throw new Exception("La solicitud POST no fue exitosa.");
+            }
+        }
+    }
+}
