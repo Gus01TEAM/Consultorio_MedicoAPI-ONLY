cd481c632ec9226938a9d839bb9a7a562b31dbb0
Gustavo Munoz <softwareadolfo@gmail.com>
2023-11-05 17:26:15 -0600

    Gustavo Munoz: Specialty service done

---
+++ Consultorio_Medico.Blazor/Data/SpecialtyService.cs
@@ -0,0 +1,88 @@
+﻿using Consultorio_Medico.BL.DTOs.RolDTO;
+using Consultorio_Medico.BL.DTOs.SpecialtiesDTO;
+using ConsultorioMedicoAPI_ONLY.DTOGenericResponse;
+using System.Text.Json;
+
+namespace Consultorio_Medico.Blazor.Data
+{
+    public class SpecialtyService
+    {
+
+        readonly HttpClient _httpClientAPI;
+
+        public SpecialtyService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientAPI = httpClientFactory.CreateClient("MEDICOAPI");
+        }
+        public async Task<DTOGenericResponse<List<SpecialtiesOutputDTO>>> Search()
+        {
+            var response = await _httpClientAPI.GetFromJsonAsync<DTOGenericResponse<List<SpecialtiesOutputDTO>>>("/api/Specialty");
+
+            if (response != null)
+            {
+                return response;
+            }
+            else
+            {
+                throw new Exception("La solicitud GET no fue exitosa.");
+            }
+        }
+        public async Task<DTOGenericResponse<SpecialtiesOutputDTO>> GetById(int id)
+        {
+            var response = await _httpClientAPI.GetFromJsonAsync<DTOGenericResponse<SpecialtiesOutputDTO>>($"/api/Specialty/{id}");
+
+            if (response != null)
+            {
+                return response;
+            }
+            else
+            {
+                throw new Exception("La solicitud GET no fue exitosa.");
+            }
+        }
+        public async Task<DTOGenericResponse<SpecialtiesOutputDTO>> Create(SpecialtiesInputDTO specialtiesInputDTO)
+        {
+            var response = await _httpClientAPI.PostAsJsonAsync("/api/Specialty", specialtiesInputDTO);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var responseBody = await response.Content.ReadAsStringAsync();
+                return JsonSerializer.Deserialize<DTOGenericResponse<SpecialtiesOutputDTO>>(responseBody);
+            }
+            else
+            {
+                throw new Exception("La solicitud POST no fue exitosa.");
+            }
+        }
+
+        public async Task<DTOGenericResponse<SpecialtiesOutputDTO>> Edit(int id, SpecialtiesInputDTO specialtiesInputDTO)
+        {
+            var response = await _httpClientAPI.PutAsJsonAsync($"/api/Specialty/{id}", specialtiesInputDTO);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var responseBody = await response.Content.ReadAsStringAsync();
+                return JsonSerializer.Deserialize<DTOGenericResponse<SpecialtiesOutputDTO>>(responseBody);
+            }
+            else
+            {
+                var errorMessage = await response.Content.ReadAsStringAsync();
+                throw new Exception($"La solicitud PUT no fue exitosa. Código de estado: {response.StatusCode}. Mensaje de error: {errorMessage}");
+            }
+        }
+        public async Task<DTOGenericResponse<SpecialtiesOutputDTO>> Delete(int id)
+        {
+            var response = await _httpClientAPI.DeleteAsync($"/api/Specialty/{id}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var responseBody = await response.Content.ReadAsStringAsync();
+                return JsonSerializer.Deserialize<DTOGenericResponse<SpecialtiesOutputDTO>>(responseBody);
+            }
+            else
+            {
+                throw new Exception("La solicitud DELETE no fue exitosa.");
+            }
+        }
+    }
+}
