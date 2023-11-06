c43cf1be9858cf13ee0a0007b2f25b5dbc17886c
LopStevn <stevenlpz03@gmail.com>
2023-11-05 19:13:32 -0600

    Steven Lopez - User Service Completado

--- Consultorio_Medico.Blazor/Data/UserService.cs
+++ Consultorio_Medico.Blazor/Data/UserService.cs
@@ -1,5 +1,6 @@
 ﻿using ConsultorioMedicoAPI_ONLY.DTOGenericResponse;
 using Consultorio_Medico.BL.DTOs.userDTO;
+using System.Text.Json;
 
 
 namespace Consultorio_Medico.Blazor.Data
@@ -15,7 +16,7 @@ namespace Consultorio_Medico.Blazor.Data
 
         public async Task<DTOGenericResponse<List<userSearchOutputDTO>>> Search()
         {
-            var response = await _httpClientAPI.GetFromJsonAsync<DTOGenericResponse<List<userSearchOutputDTO>>>("/api/patient");
+            var response = await _httpClientAPI.GetFromJsonAsync<DTOGenericResponse<List<userSearchOutputDTO>>>("/api/User");
 
             if (response != null)
             {
@@ -25,8 +26,68 @@ namespace Consultorio_Medico.Blazor.Data
             {
                 throw new Exception("La solicitud GET no fue exitosa.");
             }
+        }
 
+        public async Task<DTOGenericResponse<userSearchOutputDTO>> GetById(int id)
+        {
+            var response = await _httpClientAPI.GetFromJsonAsync<DTOGenericResponse<userSearchOutputDTO>>($"/api/User/{id}");
 
+            if (response != null)
+            {
+                return response;
+            }
+            else
+            {
+                throw new Exception("La solicitud GetById no fue exitosa.");
+            }
+        }
+
+        public async Task<DTOGenericResponse<userSearchOutputDTO>> Create(UserAddDTO createUser)
+        {
+            var response = await _httpClientAPI.PostAsJsonAsync("/api/User", createUser);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var responseBody = await response.Content.ReadAsStringAsync();
+                return JsonSerializer.Deserialize<DTOGenericResponse<userSearchOutputDTO>>(responseBody);
+            }
+            else
+            {
+                var errorMessage = await response.Content.ReadAsStringAsync();
+                throw new Exception($"La solicitud POST no fue exitosa. Código de estado: {response.StatusCode}. Mensaje de error: {errorMessage}");
+            }
+        }
+
+        public async Task<DTOGenericResponse<userSearchOutputDTO>> Edit(int id, userUpdateDTO userInput)
+        {
+            var response = await _httpClientAPI.PutAsJsonAsync($"/api/User/{id}", userInput);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var responseBody = await response.Content.ReadAsStringAsync();
+                return JsonSerializer.Deserialize<DTOGenericResponse<userSearchOutputDTO>>(responseBody);
+            }
+            else
+            {
+                var errorMessage = await response.Content.ReadAsStringAsync();
+                throw new Exception($"La solicitud PUT no fue exitosa. Código de estado: {response.StatusCode}. Mensaje de error: {errorMessage}");
+            }
+        }
+
+        public async Task<DTOGenericResponse<userSearchOutputDTO>> Delete(int id)
+        {
+            var response = await _httpClientAPI.DeleteAsync($"/api/User/{id}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var responseBody = await response.Content.ReadAsStringAsync();
+                return JsonSerializer.Deserialize<DTOGenericResponse<userSearchOutputDTO>>(responseBody);
+            }
+            else
+            {
+                var errorMessage = await response.Content.ReadAsStringAsync();
+                throw new Exception($"La solicitud PUT no fue exitosa. Código de estado: {response.StatusCode}. Mensaje de error: {errorMessage}");
+            }
         }
     }
 }
