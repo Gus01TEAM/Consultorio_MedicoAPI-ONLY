5d0d073261be84f405b502ce2fc5d7bb68accf70
LopStevn <stevenlpz03@gmail.com>
2023-11-08 21:19:44 -0600

    Steven Lopez - Vista Schedule Edit Completa y cambios en ScheduleInputDTO

--- Consultorio_Medico.BL.DTOs/ScheduleDTO/ScheduleInputDTO.cs
+++ Consultorio_Medico.BL.DTOs/ScheduleDTO/ScheduleInputDTO.cs
@@ -8,6 +8,20 @@ namespace Consultorio_Medico.BL.DTOs.ScheduleDTO
 {
     public class ScheduleInputDTO
     {
+        public ScheduleInputDTO()
+        {
+
+        }
+
+        public ScheduleInputDTO(ScheduleSearchOutPutDTO schedule)
+        {
+            SchedulesId = schedule.SchedulesId;
+            DayName = schedule.DayName;
+            StartOfShift = schedule.StartOfShift;
+            EndOfShift = schedule.EndOfShift;
+            NumberOfHours = schedule.NumberOfHours;
+        }
+
         public int SchedulesId { get; set; }
         public string DayName { get; set; }
         public decimal StartOfShift { get; set; }
