097f476b59a78c2c3d431b3fa4292382d808cdb6
LopStevn <stevenlpz03@gmail.com>
2023-11-11 17:42:57 -0600

    Steven Lopez - Vista EditPatient Completa y cambios en PatientAddDTO

--- Consultorio_Medico.BL.DTOs/PatientDTO/patientAddDTO.cs
+++ Consultorio_Medico.BL.DTOs/PatientDTO/patientAddDTO.cs
@@ -8,6 +8,20 @@ namespace Consultorio_Medico.BL.DTOs.PatientDTO
 {
     public class patientAddDTO
     {
+        public patientAddDTO()
+        {
+
+        }
+
+        public patientAddDTO(patientSearchOutputDTO pat)
+        {
+            patientId = pat.patientId;
+            Name = pat.Name;
+            LastName = pat.LastName;
+            Telefono = pat.Telefono;
+            DUI = pat.DUI;
+        }
+
         public int patientId { get; set; }   
         public string Name { get; set; }
         public string LastName { get; set; }
