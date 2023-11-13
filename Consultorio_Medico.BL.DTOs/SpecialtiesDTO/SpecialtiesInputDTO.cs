f21cd28ba3b4f29a13f8c29c322247033598fcfa
LopStevn <stevenlpz03@gmail.com>
2023-11-13 09:02:28 -0600

    Steven Lopez - Vista Edit Specialty Terminada

--- Consultorio_Medico.BL.DTOs/SpecialtiesDTO/SpecialtiesInputDTO.cs
+++ Consultorio_Medico.BL.DTOs/SpecialtiesDTO/SpecialtiesInputDTO.cs
@@ -8,6 +8,17 @@ namespace Consultorio_Medico.BL.DTOs.SpecialtiesDTO
 {
     public class SpecialtiesInputDTO
     {
+        public SpecialtiesInputDTO()
+        {
+
+        }
+
+        public SpecialtiesInputDTO(SpecialtiesOutputDTO specialtiesOutput)
+        {
+            Id = specialtiesOutput.Id;
+            Specialty = specialtiesOutput.Specialty;
+        }
+
         public int Id { get; set; }
         public string Specialty { get; set; }
     }
