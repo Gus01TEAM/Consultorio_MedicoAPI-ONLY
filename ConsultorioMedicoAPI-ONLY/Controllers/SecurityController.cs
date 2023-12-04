653e53b460d56e64c94eb1b0759786cf27fd1d28
LopStevn <stevenlpz03@gmail.com>
2023-12-04 09:00:31 -0600

    dtev lopez tiempo de token updated

--- ConsultorioMedicoAPI-ONLY/Controllers/SecurityController.cs
+++ ConsultorioMedicoAPI-ONLY/Controllers/SecurityController.cs
@@ -72,7 +72,7 @@ namespace ConsultorioMedicoAPI_ONLY.Controllers
                     var tokenDescriptor = new SecurityTokenDescriptor
                     {
                         Subject = claims,
-                        Expires = DateTime.UtcNow.AddMinutes(5),
+                        Expires = DateTime.UtcNow.AddMinutes(60),
                         SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(KeyBytes), SecurityAlgorithms.HmacSha256)
                     };
 
